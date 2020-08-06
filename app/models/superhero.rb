a1c7f86faa605101b571a783c46b89b7b8e163bc
Dwayne Weller <mr_dwayne_w@yahoo.com>
2020-08-06 06:11:03 +0000

    Automatically backed up by Learn

--- app/models/superhero.rb
+++ app/models/superhero.rb
@@ -3,7 +3,13 @@ class SuperHero
 
   @@all = []
 
-
+  def initialize(params)
+    @name = params[:name]
+    @power = params[:power]
+    @biography = params[:biography]
+    @@all << self 
+  end
+  
   def self.all
     @@all
   end
